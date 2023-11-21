3cb3d11cd868371b0740742e4ec6626afe644450
wangshouwen <wangshouwen@itcast.cn>
2023-11-21 14:56:14 +0800

    prod deploy2

--- backend/prod.sh
+++ backend/prod.sh
@@ -1,3 +1,3 @@
-mvn clean install -Dmaven.test.skip=true -P prod
+mvn clean install -Dmaven.test.skip=true
 ps aux | grep 'tomcat7:run' | grep -v grep | awk '{print $2}' | xargs kill -9
 nohup mvn tomcat7:run > backend.log &
