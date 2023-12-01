af93c57c1ba104965fe7e4ea33de02e3e6e47cfa
wangshouwen <wangshouwen@itcast.cn>
2023-12-01 14:14:58 +0800

    dc

--- deploy/docker-compose-init.sql
+++ deploy/docker-compose-init.sql
@@ -2,6 +2,6 @@
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';  
 FLUSH PRIVILEGES;
 
-CREATE DATABASE IF NOT EXISTS lottery CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS prize CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-USE lottery;
+USE prize;
